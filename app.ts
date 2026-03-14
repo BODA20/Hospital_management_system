d8bf83c6356ce6bb51b3f61ed56e1cf5346abad7
BOODA <abodydade40@gmail.com>
2026-03-14 11:58:21 +0200

    add auth routes and dotenv configuration to app setup

--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
-// src/app.ts
 import express from 'express';
 import { errorHandler } from './src/common/middleware/errorHandler';
 import usersRoutes from './src/modules/users/routes';
+import authRoutes from './src/modules/auth/auth.routes';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export const app = express();
 
 app.use(express.json());
 
 app.get('/health', (_req, res) => res.json({ ok: true }));
+app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', usersRoutes);
 
 app.use((_req, res) => {
